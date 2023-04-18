07112b75e58687a5c178314779e40df8ce9fd061
Tenshi <tenshii.tv@gmail.com>
2023-04-18 18:49:12 +0200

    init repo

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2023
+** RayTracer
+** File description:
+** main
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
