606ebede04d30ec1b827b990be0c22790e40da47
vincent <vincent.ballandi@epitech.eu>
2023-04-21 16:02:15 +0200

    RTX-29 <Add App.cpp / hpp>

---
+++ src/App.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2023
+** RayTracer
+** File description:
+** App
+*/
+
+#include "App.hpp"
+
+App::App()
+{
+}
+
+App::~App()
+{
+}
---
+++ src/App.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2023
+** RayTracer
+** File description:
+** App
+*/
+
+#pragma once
+
+class App {
+    public:
+        App();
+        ~App();
+
+    protected:
+    private:
+};
