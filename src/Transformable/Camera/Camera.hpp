0128ff9a438054747b2925e98083c0e933c1519d
tenshi <tenshii.tv@gmail.com>
2023-04-22 17:40:15 +0200

    add correct Camera.hpp

--- src/Transformable/Camera/Camera.hpp
+++ src/Transformable/Camera/Camera.hpp
@@ -4,3 +4,41 @@
 ** File description:
 ** Camera
 */
+
+#pragma once
+
+#include <vector>
+#include "ICamera.hpp"
+#include "../ATransformable.hpp"
+
+namespace Transformable {
+    namespace Camera {
+        /**
+         * @brief This class is used to create a camera
+         * @param fov Field of view
+         * @param width Number of pixels on the width
+         * @param height Number of pixels on the height
+         * @param position Position of the camera (x, y, z)
+         * @param axis Axis of the camera (in degrees)
+         * @return Camera
+         * @example Camera camera(90, 1920, 1080, {1, 2, 3}, {55, 90, 50});
+         * 
+         */
+        class Camera : public ICamera, public ATransformable {
+            public:
+                Camera(
+                    int fov = 90,
+                    int width = 1920,
+                    int height = 1080,
+                    Point3f position = {0, 0, 0},
+                    Point3f axis = {0, 0, 0}
+                );
+                std::vector<Raytracer::IVector> getVectors();
+            private:
+                int _fov;
+                int _width;
+                int _height;
+                std::vector<Raytracer::IVector> _vectors;
+        };
+    }
+}
