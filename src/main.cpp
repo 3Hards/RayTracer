31ff8c84767623ebf4f49f203677a962eddca4f1
tenshi <tenshii.tv@gmail.com>
2023-04-22 18:46:02 +0200

    add test main

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,11 @@
 */
 
 #include <iostream>
+#include "Transformable/Camera/Camera.hpp"
 
 int main()
 {
     std::cout << "Hello World!" << std::endl;
+    Transformable::Camera::Camera camera(90, 1920, 1080, {0, 0, 0}, {0, 0, 0});
     return 0;
 }
