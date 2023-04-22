7694e7d7c292d3c21c570537d57a8e6089a1f1ce
tenshi <tenshii.tv@gmail.com>
2023-04-22 17:41:06 +0200

    add camera.cpp

--- src/Transformable/Camera/Camera.cpp
+++ src/Transformable/Camera/Camera.cpp
@@ -4,3 +4,26 @@
 ** File description:
 ** Camera
 */
+
+#include "Camera.hpp"
+#include "../ATransformable.hpp"
+
+namespace Transformable {
+    namespace Camera {
+        Camera::Camera(
+            int fov,
+            int width,
+            int height,
+            Point3f position,
+            Point3f axis
+        ) : ATransformable()
+        {
+            _fov = fov;
+            _width = width;
+            _height = height;
+            _pos = position;
+            _axis = axis;
+            _type = TransformableType::Camera;
+        }
+    }
+}
