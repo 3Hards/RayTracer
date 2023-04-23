d4d7690eadf8563156593a97a1cb1960dd769c23
guillaume@isee-u.fr <guillaume@isee-u.fr>
2023-04-23 15:48:24 +0200

    Add tested vector

--- src/Raytracer/Vector.hpp
+++ src/Raytracer/Vector.hpp
@@ -13,12 +13,13 @@
 namespace Raytracer {
     class Vector : public IVector, public Transformable::ATransformable {
         public:
-            Vector();
-            void setPrimitives(std::vector<std::shared_ptr<Transformable::IPrimitive>>);
-            std::tuple<bool, Display::Color, Transformable::Point3f> run(Transformable::ILight);
+            Vector(Transformable::Point3f pos, Transformable::Point3f axis);
+            void setPrimitives(std::vector<std::shared_ptr<Transformable::Primitive::IPrimitive>>);
+            std::tuple<bool, Display::Color, Transformable::Point3f> run(Transformable::Light::ILight);
         private:
             void moveForward();
             std::tuple<bool, Display::Color> checkHit();
-            std::vector<std::shared_ptr<Transformable::IPrimitive>> _primitives;
+            std::vector<std::shared_ptr<Transformable::Primitive::IPrimitive>> _primitives;
+            float toRad(float degree);
     };
 }
