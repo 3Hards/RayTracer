7630019cf43e885aee70c9ed8cbd6bd047123ca3
Tenshi <tenshii.tv@gmail.com>
2023-04-23 18:57:53 +0200

    add test in main

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,12 @@
 
 #include <iostream>
 #include "Transformable/Camera/Camera.hpp"
+#include "Scene.hpp"
 
 int main()
 {
     std::cout << "Hello World!" << std::endl;
     Transformable::Camera::Camera camera({0, 0, 0}, {42, 0, 42}, 1920, 1080, 90);
+    Scene::Scene scene("test");
     return 0;
 }
