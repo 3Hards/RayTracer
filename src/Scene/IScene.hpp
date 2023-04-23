59e80c657835cbce6a0e6eabdc7eba01c8b90ee4
Tenshi <tenshii.tv@gmail.com>
2023-04-23 18:42:37 +0200

    change addTransformable with shared_ptr

--- src/Scene/IScene.hpp
+++ src/Scene/IScene.hpp
@@ -7,6 +7,7 @@
 
 #pragma once
 
+#include <memory>
 #include <string>
 #include "ITransformable.hpp"
 
@@ -14,7 +15,7 @@ namespace Scene {
     class IScene {
         public:
             virtual ~IScene() = default;
-            virtual void addTransformable(Transformable::ITransformable *transformable) = 0;
+            virtual void addTransformable(std::shared_ptr<Transformable::ITransformable> transformable) = 0;
             virtual void playScene(std::string const &filename) = 0;
             //virtual void addTransformation(ITransformation *transformation) = 0;
     };
--- src/Scene/Scene.hpp
+++ src/Scene/Scene.hpp
@@ -11,19 +11,33 @@
 #include <memory>
 #include "IScene.hpp"
 #include "ICamera.hpp"
+#include "IVector.hpp"
+
+namespace Transformable {
+    namespace Light {
+        class ILight {
+            public:
+                virtual ~ILight() = default;
+        };
+
+        class Light : public ILight {
+            public:
+                Light() = default;
+        };
+    }
+}
 
 namespace Scene {
     class Scene : public IScene {
         public:
             Scene(std::string const &filename);
-            void addTransformable(Transformable::ITransformable *transformable);
+            void addTransformable(std::shared_ptr<Transformable::ITransformable> transformable);
             void playScene(std::string const &filename);
         
         private:
-            void getCamera();
-
-            std::vector<std::shared_ptr<Transformable::ITransformable *>> _transformables;
             std::string _filename;
-            std::shared_ptr<ICamera> _camera;
+            std::shared_ptr<Transformable::Camera::ICamera> _camera;
+            std::shared_ptr<Transformable::Light::ILight> _light;
+            //vector of primitives...
     };
 }
