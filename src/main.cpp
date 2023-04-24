efb05503c64bf3b6ea93ded908fd3e4a3b47a0d5
Tenshi <tenshii.tv@gmail.com>
2023-04-24 16:52:32 +0200

    add tests that compile

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 
 #include <iostream>
 #include "Vector.hpp"
+
 int main()
 {
     //std::cout << "Hello World!" << std::endl;
@@ -17,6 +18,7 @@ int main()
     //return 0;
     std::vector<std::shared_ptr<Transformable::Primitive::IPrimitive>> list;
     Raytracer::Vector vector({0, 0, 0}, {0, 0, 1});
+    std::unique_ptr<Raytracer::Vector> vec = std::make_unique<Raytracer::Vector>(vector);
     Transformable::Light::ILight light;
 
     vector.setPrimitives(list);
@@ -25,6 +27,12 @@ int main()
     vector.run(light);
     pos = vector.getPos();
     std::cout << pos.x << " " << pos.y << " " << pos.z << std::endl;
-    Raytracer::IVector IVector = static_cast<Raytracer::IVector>(vector);
-    IVector.getAxis();
+
+    std::unique_ptr<Raytracer::IVector> vecI = std::make_unique<Raytracer::Vector>(vector);
+    vecI->getAxis();
+
+    std::unique_ptr<Raytracer::IVector> veccctor = nullptr;
+    veccctor = std::make_unique<Raytracer::Vector>(vector);
+
+    veccctor->getAxis();
 }
