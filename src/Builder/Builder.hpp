265ad32109f87efdabbcc352347ab1808a0b191a
vincent <vincent.ballandi@epitech.eu>
2023-04-24 16:02:47 +0200

    add: add builder hpp

---
+++ src/Builder/Builder.hpp
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2023
+** RayTracer
+** File description:
+** Builder
+*/
+
+#pragma once
+
+#include "IBuilder.hpp"
+#include <iostream>
+#include <list>
+#include "libconfig.h++"
+
+namespace Scene
+{
+    class Builder : public IBuilder
+    {
+        public:
+            Builder(std::list<libconfig::Setting *> dictionary);
+            ~Builder() = default;
+            void buildTransformation() override;
+            void buildTransformable() override;
+            // IScene getScene() override; wait add Scene class
+        private:
+            // IScene _scene; wait add Scene class
+            std::list<libconfig::Setting *> _dictionary;
+    };
+}
