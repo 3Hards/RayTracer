105df211945884a9bf886fe317add6d96c2b4fdf
guillaume abel <91884836+guillaumeAbel@users.noreply.github.com>
2023-04-26 01:17:23 +0200

    Update src/main.cpp
    Co-authored-by: TTENSHII <87119012+TTENSHII@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@ int main()
     std::shared_ptr<Transformable::Camera::ICamera> camera = std::make_shared<Transformable::Camera::Camera>(Transformable::Point3f{0, 0, -20}, Transformable::Point3f{0, 0, 1}, 10, 10, 90);
     std::shared_ptr<Material::IMaterial> material = std::make_shared<Material::FlatColor>(Display::Color{255, 0, 0});
     std::shared_ptr<Transformable::Primitive::IPrimitive> sphere = std::make_shared<Transformable::Primitive::Sphere>(Transformable::Point3f{0, 0, 0}, 5, material);
-    std::shared_ptr<Transformable::Light::ILight> light = std::make_shared<Transformable::Light::Ambiant>(Display::Color{255, 255, 255}, 0.5 , Transformable::Point3f{20, 20, 20});
+    std::shared_ptr<Transformable::Light::ILight> light = std::make_shared<Transformable::Light::Ambiant>(Display::Color{255, 255, 255}, (float)0.5 , Transformable::Point3f{20, 20, 20});
     scene->addCamera(camera);
     scene->addPrimitive(sphere);
     scene->addLight(light);
