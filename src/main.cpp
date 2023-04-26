98b7b3e926a50d724b81ae2fae68820d46dcec5f
tenshi <tenshii.tv@gmail.com>
2023-04-26 06:04:19 +0200

    add new tests

--- src/main.cpp
+++ src/main.cpp
@@ -18,9 +18,9 @@ int main()
 {
     std::cout << "Hello World!" << std::endl;
     std::unique_ptr<Scene::IScene> scene = std::make_unique<Scene::Scene>();
-    std::shared_ptr<Transformable::Camera::ICamera> camera = std::make_shared<Transformable::Camera::Camera>(Transformable::Point3f{0, 0, 0}, Transformable::Point3f{0, 0, 1}, 10, 10, 90);
+    std::shared_ptr<Transformable::Camera::ICamera> camera = std::make_shared<Transformable::Camera::Camera>(Transformable::Point3f{0, 0, 0}, Transformable::Point3f{0, 0, 1}, 20, 20, 90);
     std::shared_ptr<Material::IMaterial> material = std::make_shared<Material::FlatColor>(Display::Color{255, 0, 0});
-    std::shared_ptr<Transformable::Primitive::IPrimitive> sphere = std::make_shared<Transformable::Primitive::Sphere>(Transformable::Point3f{0, 0, 0}, 5, material);
+    std::shared_ptr<Transformable::Primitive::IPrimitive> sphere = std::make_shared<Transformable::Primitive::Sphere>(Transformable::Point3f{0, 0, 0}, 20, material);
     std::shared_ptr<Transformable::Light::ILight> light = std::make_shared<Transformable::Light::Ambiant>(Display::Color{255, 255, 255}, (float)0.5 , Transformable::Point3f{20, 20, 20});
 
     Transformation::Translation translation({0, 0, -20});
