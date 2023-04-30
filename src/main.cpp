95e4bc1e628d48025d5647a52a615de627fcffaf
Tenshi <tenshii.tv@gmail.com>
2023-04-30 17:01:43 +0200

    add plane into main

--- src/main.cpp
+++ src/main.cpp
@@ -11,29 +11,23 @@
 #include "Scene.hpp"
 #include "Ambiant.hpp"
 #include "Sphere.hpp"
+#include "Plane.hpp"
 #include "FlatColor.hpp"
 #include "Translation.hpp"
 
 int main()
 {
     std::unique_ptr<Scene::IScene> scene = std::make_unique<Scene::Scene>();
-    std::shared_ptr<Transformable::Camera::ICamera> camera = std::make_shared<Transformable::Camera::Camera>(Transformable::Point3f{0, 0, 0}, Transformable::Point3f{0, 0, 1}, 500, 500, 90);
+    std::shared_ptr<Transformable::Camera::ICamera> camera = std::make_shared<Transformable::Camera::Camera>(Transformable::Point3f{0, 0, 0}, Transformable::Point3f{0, 0, 1}, 150, 150, 90);
     std::shared_ptr<Material::IMaterial> material = std::make_shared<Material::FlatColor>(Display::Color{255, 0, 0});
-    std::shared_ptr<Material::IMaterial> material2 = std::make_shared<Material::FlatColor>(Display::Color{0, 0, 255});
-    std::shared_ptr<Material::IMaterial> material3 = std::make_shared<Material::FlatColor>(Display::Color{0, 255, 0});
-    std::shared_ptr<Transformable::Primitive::IPrimitive> sphere = std::make_shared<Transformable::Primitive::Sphere>(Transformable::Point3f{100, 0, 0}, 50, material);
-    std::shared_ptr<Transformable::Primitive::IPrimitive> sphere2 = std::make_shared<Transformable::Primitive::Sphere>(Transformable::Point3f{30, 35, 0}, 100, material2);
-    std::shared_ptr<Transformable::Primitive::IPrimitive> sphere3 = std::make_shared<Transformable::Primitive::Sphere>(Transformable::Point3f{50, 65, 0}, 100, material3);
-    std::shared_ptr<Transformable::Light::ILight> light = std::make_shared<Transformable::Light::Ambiant>(Display::Color{255, 255, 255}, (float)0.5 , Transformable::Point3f{20, 20, 20});
-
-    Transformation::Translation translation({0, 0, -20});
-    translation.applyTransformation(camera);
+    std::shared_ptr<Transformable::Primitive::IPrimitive> plane = std::make_shared<Transformable::Primitive::Plane>(Transformable::Point3f{100, 0, 0}, 25, 40, material);
+    std::shared_ptr<Transformable::Light::ILight> light = std::make_shared<Transformable::Light::Ambiant>(Display::Color{255, 255, 255}, (float)0.5 , Transformable::Point3f{0, 0, -5});
 
     scene->addCamera(camera);
-    scene->addPrimitive(sphere);
-    scene->addPrimitive(sphere2);
-    scene->addPrimitive(sphere3);
+    scene->addPrimitive(plane);
     scene->addLight(light);
+    std::cout << "PLAY SCENE" << std::endl;
     scene->playScene("test");
+    std::cout << "END SCENE" << std::endl;
     return 0;
 }
