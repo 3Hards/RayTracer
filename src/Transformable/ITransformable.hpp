b3f4a16e3827cf4b1ce2730821e0176f392749f9
tenshi <tenshii.tv@gmail.com>
2023-05-07 20:06:18 +0200

    add some vector methods

--- src/Transformable/ITransformable.hpp
+++ src/Transformable/ITransformable.hpp
@@ -7,12 +7,29 @@
 
 #pragma once
 
+#include <cmath>
+
 namespace Transformable {
     struct Point3d {
         public:
             double x;
             double y;
             double z;
+
+        Point3d normalize() const {
+            double length = sqrt(x * x + y * y + z * z);
+            return {x / length, y / length, z / length};
+        }
+        double dot(Point3d v) const {
+            return x * v.x + y * v.y + z * v.z;
+        }
+        Point3d cross(Point3d v) const {
+            return {y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x};
+        }
+        double length() const {
+            return sqrt(x * x + y * y + z * z);
+        }
+
     };
 
     Point3d operator-(Point3d a, Point3d b);
