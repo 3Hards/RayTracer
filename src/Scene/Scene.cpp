e8c873c139b9b381011f32f4a2982fc2bd9703a0
tenshi <tenshii.tv@gmail.com>
2023-05-08 20:08:23 +0200

    clean scene

--- src/Scene/Scene.cpp
+++ src/Scene/Scene.cpp
@@ -12,85 +12,13 @@
 #include "IVector.hpp"
 #include "Vector.hpp"
 #include "cmath"
+#include "3DMatrix.hpp"
 
 #include <iostream>
 
 #ifndef M_PI
     #define M_PI 3.14159265358979323846
 #endif
-
-namespace Transformable {
-
-    class Matrix3d {
-    private:
-        double data[3][3];
-
-    public:
-        Matrix3d() {
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    data[i][j] = (i == j) ? 1.0 : 0.0;
-                }
-            }
-        }
-
-        Matrix3d rotateX(double angle) {
-            Matrix3d rotationMatrix;
-            rotationMatrix.data[1][1] = std::cos(angle);
-            rotationMatrix.data[1][2] = -std::sin(angle);
-            rotationMatrix.data[2][1] = std::sin(angle);
-            rotationMatrix.data[2][2] = std::cos(angle);
-            return (*this) * rotationMatrix;
-        }
-
-        Matrix3d rotateY(double angle) {
-            Matrix3d rotationMatrix;
-            rotationMatrix.data[0][0] = std::cos(angle);
-            rotationMatrix.data[0][2] = std::sin(angle);
-            rotationMatrix.data[2][0] = -std::sin(angle);
-            rotationMatrix.data[2][2] = std::cos(angle);
-            return (*this) * rotationMatrix;
-        }
-
-        Matrix3d rotateZ(double angle) {
-            Matrix3d rotationMatrix;
-            rotationMatrix.data[0][0] = std::cos(angle);
-            rotationMatrix.data[0][1] = -std::sin(angle);
-            rotationMatrix.data[1][0] = std::sin(angle);
-            rotationMatrix.data[1][1] = std::cos(angle);
-            return (*this) * rotationMatrix;
-        }
-
-        Matrix3d operator*(const Matrix3d& other) const {
-            Matrix3d result;
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    result.data[i][j] = 0.0;
-                    for (int k = 0; k < 3; k++) {
-                        result.data[i][j] += data[i][k] * other.data[k][j];
-                    }
-                }
-            }
-            return result;
-        }
-
-        double& operator()(int i, int j) {
-            return data[i][j];
-        }
-
-        const double& operator()(int i, int j) const {
-            return data[i][j];
-        }
-    };
-
-    Point3d operator*(const Matrix3d& matrix, const Point3d& point) {
-        Point3d result;
-        result.x = matrix(0, 0) * point.x + matrix(0, 1) * point.y + matrix(0, 2) * point.z;
-        result.y = matrix(1, 0) * point.x + matrix(1, 1) * point.y + matrix(1, 2) * point.z;
-        result.z = matrix(2, 0) * point.x + matrix(2, 1) * point.y + matrix(2, 2) * point.z;
-        return result;
-    }
-}
 
 namespace Scene {
 
@@ -114,38 +42,6 @@ namespace Scene {
         _transformations.push_back(transformation);
     }
 
-    Transformable::Point3d getRayDirection(int x, int y, std::shared_ptr<Transformable::Camera::ICamera> camera)
-    {
-        // camres = 1920 1080
-        // camFov = 90
-        // camPos = 0 0 0
-        // camDirection = 0 0 1
-        // camUp = 0 1 0
-
-        double angleXRad = camera->getAxis().x * M_PI / 180;
-        double angleYRad = camera->getAxis().y * M_PI / 180;
-        double angleZRad = camera->getAxis().z * M_PI / 180;
-
-        double fovScale = std::tan((90 * M_PI / 180) / 2);
-        double aspectRatio = 600.0 / 400.0;
-        
-        double xx = (2 * ((x + 0.5) / 600) - 1) * fovScale * aspectRatio * -1;
-        double yy = (1 - 2 * ((y + 0.5) / 400)) * fovScale;
-
-        Transformable::Point3d cameraDirection = {1, 0, 0};
-        Transformable::Point3d cameraUp = {0, 0, 1};
-
-        Transformable::Matrix3d rotationMatrix = Transformable::Matrix3d().rotateY(angleYRad).rotateX(angleXRad).rotateZ(angleZRad);
-        Transformable::Point3d cameraDirectionRotated = rotationMatrix * cameraDirection;
-        Transformable::Point3d cameraUpRotated = rotationMatrix * cameraUp;
-
-        Transformable::Point3d cameraRight = cameraDirectionRotated.cross(cameraUpRotated);
-
-        Transformable::Point3d rayDirection = cameraDirectionRotated + xx * cameraRight + yy * cameraUpRotated;
-        return rayDirection;
-    }
-
-
     void Scene::computeVectors(unsigned int camWidth, unsigned int camHeight, Transformable::Point3d camPos)
     {
         unsigned int i = 0;
@@ -157,7 +53,7 @@ namespace Scene {
             for (unsigned int x = 0; x < camWidth; x++) {
                 Display::Point2i pixelPos = Display::Point2i{(int)x, (int)y};
                 vector->setPos(camPos);
-                vector->setAxis(getRayDirection((int)x, (int)y, _cameras[0]));
+                vector->setAxis(_cameras[0]->getRayAxis((int)x, (int)y));
                 Raytracer::LightCalculator calculator(vector, _lights[0]);
                 libGraphicHandler.addPixelToImage(createPixel(calculator.computePixel(), pixelPos));
                 i++;
