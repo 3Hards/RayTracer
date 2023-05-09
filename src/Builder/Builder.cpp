44d96e023b21bc42b1c37e899a146cd2c1283f78
vincent <vincent.ballandi@epitech.eu>
2023-05-09 17:54:18 +0200

    fix: fix builder.cpp

--- src/Builder/Builder.cpp
+++ src/Builder/Builder.cpp
@@ -36,64 +36,68 @@ namespace Scene
     {
         std::string type;
         setting.lookupValue("type", type);
+        std::cout << "type: " << type << std::endl;
         _map.at(type)(setting);
     }
 
     void Builder::createCamera(libconfig::Setting& setting)
     {
-            double x, y, z;
+            int x, y, z;
             int fov;
             const libconfig::Setting &axis = setting["axis"];
-            double ax, ay, az;
+            int ax, ay, az;
             const libconfig::Setting &res = setting["resolution"];
-            double rx, ry;
-            const libconfig::Setting &trans = setting["transformation"];
+            int rx, ry;
             std::string transType;
-            double tx, ty, tz;
 
-            setting.lookupValue("x", x);
-            setting.lookupValue("y", y);
-            setting.lookupValue("z", z);
-            setting.lookupValue("fov", fov);
-            axis.lookupValue("x", ax);
-            axis.lookupValue("y", ay);
-            axis.lookupValue("z", az);
-            res.lookupValue("x", rx);
-            res.lookupValue("y", ry);
-            trans.lookupValue("type", transType);
-            trans.lookupValue("x", tx);
-            trans.lookupValue("y", ty);
-            trans.lookupValue("z", tz);
-            std::shared_ptr<Transformable::Camera::ICamera> camera = std::make_shared<Transformable::Camera::Camera>(Transformable::Point3d{x, y, z}, Transformable::Point3d{ax, ay, az}, rx, ry, fov);
+            x = setting.lookup("x");
+            y = setting.lookup("y");
+            z = setting.lookup("z");
+            fov = setting.lookup("fov");
+            ax = axis.lookup("x");
+            ay = axis.lookup("y");
+            az = axis.lookup("z");
+            rx = res.lookup("x");
+            ry = res.lookup("y");
+            std::cout << "x: " << x << " y: " << y << " z: " << z << " fov: " << fov << std::endl;
+            std::shared_ptr<Transformable::Camera::ICamera> camera = std::make_shared<Transformable::Camera::Camera>(Transformable::Point3d{(double)x, (double)y, (double)z}, Transformable::Point3d{(double)ax, (double)ay, (double)az}, (double)rx, (double)ry, (double)fov);
+            std::cout << "Camera created" << std::endl;
             _scene->addCamera(camera);
     }
 
     void Builder::createSphere(libconfig::Setting& setting)
     {
-            double x, y, z, r;
-            const libconfig::Setting& color = setting["color"];
-            int cr, cg, cb;
-            color.lookupValue("r", cr);
-            color.lookupValue("g", cg);
-            color.lookupValue("b", cb);
-            setting.lookupValue("x", x);
-            setting.lookupValue("y", y);
-            setting.lookupValue("z", z);
-            setting.lookupValue("r", r);
-            std::shared_ptr<Material::IMaterial> material = std::make_shared<Material::FlatColor>(Display::Color{cr, cg, cb});
-            std::shared_ptr<Transformable::Primitive::IPrimitive> sphere = std::make_shared<Transformable::Primitive::Sphere>(Transformable::Point3d{x, y, z}, r, material);
+            int x, y, z;
+            std::string type = setting.lookup("type");
+            x = setting.lookup("x");
+            y = setting.lookup("y");
+            z = setting.lookup("z");
+            int r = setting.lookup("r");
+            const libconfig::Setting& color = setting.lookup("color");
+            int red = color.lookup("r");
+            int green = color.lookup("g");
+            int blue = color.lookup("b");
+            std::cout << "x: " << x << " y: " << y << " z: " << z << " r: " << r << " red: " << red << " green: " << green << " blue: " << blue << std::endl;
+            std::shared_ptr<Material::IMaterial> material = std::make_shared<Material::FlatColor>(Display::Color{red, green, blue});
+            std::shared_ptr<Transformable::Primitive::IPrimitive> sphere = std::make_shared<Transformable::Primitive::Sphere>(Transformable::Point3d{(double)x, (double)y, (double)z}, r, material);
+            std::cout << "Sphere created" << std::endl;
             _scene->addPrimitive(sphere);
     }
 
     void Builder::createAmbientLight(libconfig::Setting& setting)
     {
-            double x, y, z;
+            int x, y, z;
             double brightness;
-            setting.lookupValue("x", x);
-            setting.lookupValue("y", y);
-            setting.lookupValue("z", z);
-            setting.lookupValue("brightness", brightness);
-            std::shared_ptr<Transformable::Light::ILight> light = std::make_shared<Transformable::Light::Ambient>(Display::Color{0, 0, 0}, brightness, Transformable::Point3d{x, y, z});
+            x = setting.lookup("x");
+            std::cout << "x: " << x << std::endl;
+            y = setting.lookup("y");
+            z = setting.lookup("z");
+            std::cout << "z: " << z << std::endl;
+            brightness = setting.lookup("brightness");
+            std::cout << "brightness: " << brightness << std::endl;
+            std::cout << "x: " << x << " y: " << y << " z: " << z << " brightness: " << brightness << std::endl;
+            std::shared_ptr<Transformable::Light::ILight> light = std::make_shared<Transformable::Light::Ambient>(Display::Color{0, 0, 0}, brightness, Transformable::Point3d{(double)x, (double)y, (double)z});
+            std::cout << "Ambient light created" << std::endl;
             _scene->addLight(light);
     }
 }
