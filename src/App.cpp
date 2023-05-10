73fb75f2f3bdf627b2e66b2e707295114d8be5ee
vincent <vincent.ballandi@epitech.eu>
2023-05-10 16:18:55 +0200

    add: add app.cpp

--- src/App.cpp
+++ src/App.cpp
@@ -6,6 +6,7 @@
 */
 
 #include "App.hpp"
+#include "Director.hpp"
 
 App::App()
 {
@@ -14,3 +15,17 @@ App::App()
 App::~App()
 {
 }
+
+int App::run(char **av)
+{
+    if (av[1] == NULL) return 84;
+    try {
+        Scene::Director director(av[1]);
+        director.playScene();
+    } catch (std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        return 84;
+    }
+    return 0;
+}
+
