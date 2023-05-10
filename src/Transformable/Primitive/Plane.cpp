1fe16f02973b44776a666d7f985bb0191ad53c32
tenshi <tenshii.tv@gmail.com>
2023-05-11 00:12:51 +0200

    fix plane calculs

--- src/Transformable/Primitive/Plane.cpp
+++ src/Transformable/Primitive/Plane.cpp
@@ -15,60 +15,68 @@
 namespace Transformable {
     namespace Primitive {
 
-        Transformable::Point3d rotateVector(const Transformable::Point3d& vector, double angleX, double angleY, double angleZ)
-        {
-            Transformable::Point3d rotatedX = {
-                vector.x, std::cos(angleX) * vector.y - std::sin(angleX) * vector.z,
-                std::sin(angleX) * vector.y + std::cos(angleX) * vector.z
-            };
-            Transformable::Point3d rotatedY = {
-                std::cos(angleY) * rotatedX.x + std::sin(angleY) * rotatedX.z, rotatedX.y,
-                -std::sin(angleY) * rotatedX.x + std::cos(angleY) * rotatedX.z
-            };
-            Transformable::Point3d rotatedZ = {
-                std::cos(angleZ) * rotatedY.x - std::sin(angleZ) * rotatedY.y,
-                std::sin(angleZ) * rotatedY.x + std::cos(angleZ) * rotatedY.y,
-                rotatedY.z
-            };
-            return rotatedZ;
-        }
-
         Plane::Plane(Point3d pos, double length, double width, Point3d axis, std::shared_ptr<Material::IMaterial> material)
             : Transformable::Primitive::APrimitive(material, pos, axis), _length(length), _width(width)
         {
-            _normal = {0.0, 0.0, 1.0};
+            _normal = {0.0, 0.0, 0.0};
 
             double angleX = getAxis().x * M_PI / 180.0;
             double angleY = getAxis().y * M_PI / 180.0;
             double angleZ = getAxis().z * M_PI / 180.0;
-           
-            _normal = rotateVector(_normal, angleX, angleY, angleZ);
-            Point3d dimensions = {_length, _width, 0.0};
-            dimensions = rotateVector(dimensions, angleX, angleY, angleZ);
-            _length = dimensions.x;
-            _width = dimensions.y;
-            double lengthh = _normal.length();
-            _normal = Point3d {_normal.x / lengthh, _normal.y / lengthh, _normal.z / lengthh};
+
+            Point3d pointB = getPos();
+            Point3d pointR = {getPos().x + _length / 2.0, getPos().y, getPos().z};
+            Point3d pointS = {getPos().x, getPos().y + _width / 2.0, getPos().z};
+
+            Point3d rotatedPointB = rotatePoint3D(pointB, angleX, angleY, angleZ);
+            Point3d rotatedPointR = rotatePoint3D(pointR, angleX, angleY, angleZ);
+            Point3d rotatedPointS = rotatePoint3D(pointS, angleX, angleY, angleZ);
+
+            Point3d vectorBR = rotatedPointR - rotatedPointB;
+            Point3d vectorBS = rotatedPointS - rotatedPointB;
+
+            _normal = vectorBR.cross(vectorBS);
+            _normal.normalize();
+        }
+
+        Point3d Plane::rotatePoint3D(const Point3d& point, double angleX, double angleY, double angleZ)
+        {
+            Point3d res = {0, 0, 0};
+
+            double rotatedX = point.x;
+            double rotatedY = std::cos(angleX) * point.y - std::sin(angleX) * point.z;
+            double rotatedZ = std::sin(angleX) * point.y + std::cos(angleX) * point.z;
+            double computeX = std::cos(angleY) * rotatedX + std::sin(angleY) * rotatedZ;
+            double computeY = rotatedY;
+            double computeZ = -std::sin(angleY) * rotatedX + std::cos(angleY) * rotatedZ;
+
+            res = {
+                std::cos(angleZ) * computeX - std::sin(angleZ) * computeY,
+                std::sin(angleZ) * computeX + std::cos(angleZ) * computeY,
+                computeZ
+            };
+
+            return res;
         }
 
         bool Plane::checkHit(std::shared_ptr<Raytracer::IVector> vector)
         {
             Point3d vectorPos = vector->getPos();
             Point3d vectorAxis = vector->getAxis();
-            Point3d rotatedVectorPos = rotateVector(vectorPos - getPos(), -getAxis().x, -getAxis().y, -getAxis().z);
-            Point3d rotatedVectorAxis = rotateVector(vectorAxis, -getAxis().x, -getAxis().y, -getAxis().z);
-            double dotProduct = rotatedVectorAxis.dot(_normal);
+            double dotProduct = vectorAxis.dot(_normal);
 
             if (dotProduct == 0) {
                 return false;
             }
-            double t = -rotatedVectorPos.dot(_normal) / dotProduct;
+            double t = (_normal.dot(getPos() - vectorPos)) / dotProduct;
             if (t < 0) {
                 return false;
             }
             Point3d hitPos = vectorPos + vectorAxis * t;
-            Point3d localHitPos = rotateVector(hitPos - getPos(), -getAxis().x, -getAxis().y, -getAxis().z);
-            if (localHitPos.x <= _length / 2.0 && localHitPos.x >= -_length / 2.0 && localHitPos.y <= _width / 2.0 && localHitPos.y >= -_width / 2.0) {
+            Point3d localHitPos = hitPos - getPos();
+            Point3d rotatedHitPos = rotatePoint3D(localHitPos, -getAxis().x, -getAxis().y, -getAxis().z);
+            if (rotatedHitPos.x <= _length / 2.0 && rotatedHitPos.x >= -_length / 2.0 &&
+                rotatedHitPos.y <= _width / 2.0 && rotatedHitPos.y >= -_width / 2.0) {
                 _lastHittedVector = vector;
                 vector->setPos(hitPos);
                 return true;
