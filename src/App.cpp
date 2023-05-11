be1395194dad85cd8c17e13a4e53cac18b232894
guillaumeAbel <guillaume.abel@epitech.eu>
2023-05-11 02:45:53 +0200

    Add functionnal scene changing

--- src/App.cpp
+++ src/App.cpp
@@ -6,19 +6,33 @@
 */
 
 #include <iostream>
-#include <list>
 #include "App.hpp"
 #include "Director.hpp"
 
-int App::run(std::list<std::string> args)
+App::App(std::vector<std::string> args) : _args(args), _pathId(0)
+{}
+
+void App::incrementPathId()
 {
-    if (args.size() < 1) {
+    if (_pathId == (int)_args.size() - 1) {
+        _pathId = 0;
+    } else {
+        _pathId += 1;
+    }
+}
+
+int App::run()
+{
+    if (_args.size() < 1) {
         std::cerr << "Usage: ./raytracer [config_file]" << std::endl;
         return 84;
     }
     try {
-        Scene::Director director(args.back());
-        director.playScene();
+        Scene::Director director(_args[0]);
+        while (director.playScene()) {
+            incrementPathId();
+            director = Scene::Director(_args[_pathId]);
+        }
         return 0;
     } catch (std::exception &e) {
         std::cerr << e.what() << std::endl;
--- src/App.hpp
+++ src/App.hpp
@@ -8,10 +8,16 @@
 #pragma once
 
 #include <iostream>
-#include <list>
+#include <vector>
 #include "Director.hpp"
 
 class App {
     public:
-        int run(std::list<std::string> args);
+        App(std::vector<std::string> args);
+        int run();
+    private:
+        void incrementPathId();
+
+        std::vector<std::string> _args;
+        int _pathId;
 };
